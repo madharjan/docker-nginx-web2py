6851237207e0855df85509ecebc0292359005c17
Madharjan <madhav.maharjan@gmail.com>
2019-05-12 22:40:31 +0800

    Using git branch to build 2.14.6

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,27 +17,26 @@ cp ${NGINX_CONFIG_PATH}/default-web2py.conf /config/etc/nginx-web2py/conf.d/defa
 ## Install uwsgi and runit service
 /build/services/uwsgi/uwsgi.sh
 
-if [ "${WEB2PY_MIN}" == false ]; then
-  apt-get install -y --no-install-recommends git-core
-  pip install gitpython
-fi
+apt-get install -y --no-install-recommends git-core
+pip install gitpython
 
 ## Install Web2py
-cd /opt
-wget https://github.com/web2py/web2py/archive/R-2.14.6.zip
-mkdir tmp
-unzip R-2.14.6.zip -d tmp
+mkdir -p /opt/tmp
+cd /opt/tmp
+git clone https://github.com/web2py/web2py.git --depth 1 --branch R-2.14.6 --single-branch web2py
+cd web2py
+git submodule update --init --recursive
+cd ../../
 
 if [ "${WEB2PY_MIN}" == true ]; then
-  cd tmp/web2py-R-2.14.6
+  cd tmp/web2py
   python scripts/make_min_web2py.py ../../tmp/web2py-min
   mv ../../tmp/web2py-min ../../web2py
   cd ../../
 else
-  mv tmp/web2py-R-2.14.6 web2py
+  mv tmp/web2py web2py
 fi
 
-rm R-2.14.6.zip
 rm -rf tmp
 mv web2py/handlers/wsgihandler.py web2py/wsgihandler.py
 
